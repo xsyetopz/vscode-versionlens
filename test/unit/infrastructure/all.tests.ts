ce60df44a48f3d3d08d51accbfffff8af03293eb
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-11 17:43:06 +0000

    Moved http tests into domain

--- test/unit/infrastructure/all.tests.ts
+++ test/unit/infrastructure/all.tests.ts
@@ -1,7 +1,6 @@
 export * as DotNetProvider from '#test/unit/providers/dotnet';
 export * as MavenProvider from '#test/unit/providers/maven';
 export * as NpmProvider from '#test/unit/providers/npm';
-export * as Http from './http/requestLightClient.tests';
 export * as Parser from './parsers/all.tests';
 export * as Process from './process/promiseSpawnClient.tests';
 export * as Watcher from './watcher/packageFileWatcher.tests';
