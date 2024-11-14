6da98cbf3821539df20920f4ec76346883de8d02
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-14 00:41:23 +0000

    Moved pub defintions into pub/defintions.ts

--- src/domain/providers/pub/definitions.ts
+++ src/domain/providers/pub/definitions.ts
@@ -1,17 +1,21 @@
+export enum PubFeatures {
+  Caching = 'pub.caching',
+  Http = 'pub.http',
+  DependencyProperties = 'pub.dependencyProperties',
+  ApiUrl = 'pub.apiUrl',
+  FilePatterns = 'pub.files',
+  OnSaveChangesTask = 'pub.onSaveChanges',
+  PrereleaseTagFilter = 'pub.prereleaseTagFilter',
+}
+
 import { CachingOptions } from '#domain/caching';
 import { HttpOptions, JsonHttpClient } from '#domain/clients';
 import { PubClient, PubConfig } from '#domain/providers/pub';
 
 export interface IPubServices {
-
   pubCachingOpts: CachingOptions;
-
   pubHttpOpts: HttpOptions;
-
   pubConfig: PubConfig;
-
   pubJsonClient: JsonHttpClient;
-
   pubClient: PubClient;
-
 }
--- src/domain/providers/pub/definitions/ePubFeatures.ts
+++
@@ -1,9 +0,0 @@
-export enum PubFeatures {
-  Caching = 'pub.caching',
-  Http = 'pub.http',
-  DependencyProperties = 'pub.dependencyProperties',
-  ApiUrl = 'pub.apiUrl',
-  FilePatterns = 'pub.files',
-  OnSaveChangesTask = 'pub.onSaveChanges',
-  PrereleaseTagFilter = 'pub.prereleaseTagFilter',
-}
