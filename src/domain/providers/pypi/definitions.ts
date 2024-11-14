6a9278f7c8e942e9a927b4bdb852f75591f269a6
pflannery <61407-pflannery@users.noreply.gitlab.com>
2024-11-14 00:48:11 +0000

    Moved pypi defintions into pypi/defintions.ts

--- src/domain/providers/pypi/definitions.ts
+++ src/domain/providers/pypi/definitions.ts
@@ -2,16 +2,20 @@ import { CachingOptions } from '#domain/caching';
 import { HttpOptions, IHttpClient } from '#domain/clients';
 import { PypiClient, PypiConfig } from '#domain/providers/pypi';
 
+export enum PypiFeatures {
+  Caching = 'pypi.caching',
+  Http = 'pypi.http',
+  DependencyProperties = 'pypi.dependencyProperties',
+  ApiUrl = 'pypi.apiUrl',
+  FilePatterns = 'pypi.files',
+  OnSaveChangesTask = 'pypi.onSaveChanges',
+  PrereleaseTagFilter = 'pypi.prereleaseTagFilter',
+}
+
 export interface IPypiService {
-
   pypiCachingOpts: CachingOptions;
-
   pypiHttpOpts: HttpOptions;
-
   pypiConfig: PypiConfig;
-
   pypiHttpClient: IHttpClient;
-
   pypiClient: PypiClient;
-
 }
--- src/domain/providers/pypi/definitions/ePypiFeatures.ts
+++
@@ -1,9 +0,0 @@
-export enum PypiFeatures {
-  Caching = 'pypi.caching',
-  Http = 'pypi.http',
-  DependencyProperties = 'pypi.dependencyProperties',
-  ApiUrl = 'pypi.apiUrl',
-  FilePatterns = 'pypi.files',
-  OnSaveChangesTask = 'pypi.onSaveChanges',
-  PrereleaseTagFilter = 'pypi.prereleaseTagFilter',
-}
